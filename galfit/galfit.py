da9f1ec886d54883a036d6b21265fcaaecd2ca6f
Wilfried Mercier <wilfried.mercier58@gmail.com>
2019-10-25 12:14:15 +0200

    changed fullKeys dict

--- galfit/galfit.py
+++ galfit/galfit.py
@@ -9,7 +9,7 @@ Functions related to automating galfit modelling.
 """
 
 from wilfried.galfit.models import gendeVaucouleur, genEdgeOnDisk, genExpDisk, genFerrer, genGaussian, genKing, genMoffat, genNuker, genPSF, genSersic, genSky
-from wilfried.utilities.dictionnaries import checkDictKeys, checkInDict, removeKeys, setDict
+from wilfried.utilities.dictionnaries import checkDictKeys, removeKeys, setDict
 from wilfried.utilities.strings import putStringsTogether, toStr, maxStringsLen
 from os.path import isdir
 
@@ -18,54 +18,42 @@ from os.path import isdir
 #          Global variables              #
 ##########################################
 
-# If you change a variable name in a function declaration, you must also change the name here
-fullKeys = {'header': {'mandatory':['outputImage', 'xmin', 'xmax', 'ymin', 'ymax'], 
-                       'optional':[['inputImage', 'sigmaImage', 'psfImage', 'maskImage', 'couplingFile', 'psfSamplingFactor', 'zeroPointMag', 'arcsecPerPixel', 'sizeConvX', 'sizeConvY', 'displayType', 'option'],
-                                   ["none", "none", "none", "none", "none", 1, 30.0, [0.03, 0.03], None, None, "regular", 0]]},
+# If you change a parameter name in a function declaration, you must also change the name here
+fullKeys = {'header': {'parameters':['outputImage', 'xmin', 'xmax', 'ymin', 'ymax', 'inputImage', 'sigmaImage', 'psfImage', 'maskImage', 'couplingFile', 'psfSamplingFactor', 'zeroPointMag', 'arcsecPerPixel', 'sizeConvX', 'sizeConvY', 'displayType', 'option'], 
+                       'default':['output.fits', 0, 100, 0, 100, "none", "none", "none", "none", "none", 1, 30.0, [0.03, 0.03], None, None, "regular", 0]},
                                    
-            'deVaucouleur': {'mandatory':['posX', 'posY', 'magTot', 're'], 
-                             'optional':[['bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
-                                         [1.0, 0.0, False, [], None, False]]},
+            'deVaucouleur': {'parameters':['posX', 'posY', 'magTot', 're', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
+                             'default':[50, 50, 25.0, 10.0, 1.0, 0.0, False, [], None, False]},
                                          
-            'edgeOnDisk': {'mandatory':['posX', 'posY', 'mu', 'diskScaleLength', 'diskScaleHeight'], 
-                           'optional':[['PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
-                                       [0.0, False, [], None, False]]},
+            'edgeOnDisk': {'parameters':['posX', 'posY', 'mu', 'diskScaleLength', 'diskScaleHeight', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
+                           'default':[50, 50, 20.0, 10.0, 2.0, 0.0, False, [], None, False]},
                                        
-            'expDisk': {'mandatory':['posX', 'posY', 'magTot', 'rs'], 
-                        'optional':[['bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
-                                    [1.0, 0.0, False, [], None, False]]},
+            'expDisk': {'parameters':['posX', 'posY', 'magTot', 'rs', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
+                        'default':[50, 50, 25.0, 8.0, 1.0, 0.0, False, [], None, False]},
                                     
-            'ferrer': {'mandatory':['posX', 'posY', 'mu', 'rt'], 
-                       'optional':[['alphaFerrer', 'betaFerrer', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
-                                   [3.0, 2.5, 1.0, 0.0, False, [], None, False]]},
+            'ferrer': {'parameters':['posX', 'posY', 'mu', 'rt', 'alphaFerrer', 'betaFerrer', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
+                       'default':[50, 50, 20.0, 5.0, 3.0, 2.5, 1.0, 0.0, False, [], None, False]},
                                    
-            'gaussian': {'mandatory':['posX', 'posY', 'magTot', 'FWHM'], 
-                         'optional':[['bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
-                                    [1.0, 0.0, False, [], None, False]]},
+            'gaussian': {'parameters':['posX', 'posY', 'magTot', 'FWHM', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
+                         'default':[50, 50, 25.0, 3.0, 1.0, 0.0, False, [], None, False]},
 
-            'king': {'mandatory':['posX', 'posY', 'mu0', 'rc', 'rt'], 
-                     'optional':[['powerlaw', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
-                                [2.0, 1.0, 0.0, False, [], None, False]]},
+            'king': {'parameters':['posX', 'posY', 'mu0', 'rc', 'rt', 'powerlaw', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
+                     'default':[50, 50, 20.0, 3.0, 30.0, 2.0, 1.0, 0.0, False, [], None, False]},
 
-            'moffat': {'mandatory':['posX', 'posY', 'magTot', 'FWHM'], 
-                       'optional':[['powerlaw', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
-                                  [1.0, 1.0, 0.0, False, [], None, False]]},
+            'moffat': {'parameters':['posX', 'posY', 'magTot', 'FWHM', 'powerlaw', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
+                       'default':[50, 50, 25.0, 3.0, 1.0, 1.0, 0.0, False, [], None, False]},
 
-            'nuker': {'mandatory':['posX', 'posY', 'mu', 'rb'], 
-                      'optional':[['alpha', 'beta', 'gamma', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
-                                 [1.0, 0.5, 0.7, 1.0, 0.0, False, [], None, False]]},
+            'nuker': {'parameters':['posX', 'posY', 'mu', 'rb', 'alpha', 'beta', 'gamma', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
+                      'default':[50, 50, 20.0, 10.0, 1.0, 0.5, 0.7, 1.0, 0.0, False, [], None, False]},
 
-            'psf': {'mandatory':['posX', 'posY', 'magTot'], 
-                    'optional':[['skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
-                               [False, [], None, False]]},
+            'psf': {'parameters':['posX', 'posY', 'magTot', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
+                    'default':[50, 50, 25.0, False, [], None, False]},
                     
-            'sersic': {'mandatory':['posX', 'posY', 'magTot', 're'], 
-                       'optional':[['n', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
-                                  [4, 1.0, 0.0, False, [], None, False]]},
+            'sersic': {'parameters':['posX', 'posY', 'magTot', 're', 'n', 'bOvera', 'PA', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
+                       'default':[50, 50, 25.0, 10.0, 4, 1.0, 0.0, False, [], None, False]},
                        
-            'sky': {'mandatory':['background', 'xGradient', 'yGradient'], 
-                    'optional':[['skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
-                               [False, [], None, False]]}
+            'sky': {'parameters':['background', 'xGradient', 'yGradient', 'skipComponentInResidual', 'fixedParams', 'comments', 'noComments'], 
+                    'default':[1.0, 0.0, 0.0, False, [], None, False]}
             }
             
 # Keeping track of the model functions
@@ -114,13 +102,13 @@ def genFeedme(header, listProfiles):
                     - bending modes 1 and 3 with values 0.2 and 0.4 respectively added to the Nuker profile
                     
                 Then one may write something like
-                    header  = {'outputImage':'output.fits', 'zeroPointMag':25.5}
-                    sersic  = {'name':'sersic', 'posX':45, 'posY':56, 'magTot':25, 're':10, 'n':1, 'PA':100, 'isFixed':['n']}
+                    >>> header  = {'outputImage':'output.fits', 'zeroPointMag':25.5}
+                    >>> sersic  = {'name':'sersic', 'posX':45, 'posY':56, 'magTot':25, 're':10, 'n':1, 'PA':100, 'isFixed':['n']}
                     
-                    bending = {'listModes':[1, 3], 'listModesValues':[0.2, 0.4]}
-                    nuker  = {'name':'nuker', 'gamma':1.5, 'mu':20.1, 'bending':bending}
+                    >>> bending = {'listModes':[1, 3], 'listModesValues':[0.2, 0.4]}
+                    >>> nuker  = {'name':'nuker', 'gamma':1.5, 'mu':20.1, 'bending':bending}
                     
-                    genFeedme(header, [sersic, nuker])
+                    >>> genFeedme(header, [sersic, nuker])
                 
     Return a full galfit.feedme configuration with header and body as formatted text.
     """
@@ -136,17 +124,14 @@ def genFeedme(header, listProfiles):
         except KeyError:
             raise KeyError("key 'name' is not provided in one of the dictionnaries.")
         
-        # Check that the given keys contain at the very least the mandatory ones
-        checkInDict(dic, keys=fullKeys[dic['name']]['mandatory'] + ['name'], dictName='header or listProfiles')
-        
         # Check that the given dictionnary only has valid keys
-        checkDictKeys(dic, keys=fullKeys[dic['name']]['mandatory'] + fullKeys[dic['name']]['optional'][0] + ['name'], dictName='header or listProfiles')
+        checkDictKeys(dic, keys=fullKeys[dic['name']]['parameters'] + ['name'], dictName='header or listProfiles')
     
         # Set default values if not provided
         if pos==0:
-            header = setDict(dic, keys=fullKeys[dic['name']]['mandatory'] + fullKeys[dic['name']]['optional'][0] + ['name'], default=fullKeys[dic['name']]['mandatory'] + fullKeys[dic['name']]['optional'][1] + [dic['name']])
+            header = setDict(dic, keys=fullKeys[dic['name']]['parameters'] + ['name'], default=fullKeys[dic['name']]['default'] + [dic['name']])
         else:
-            listProfiles[pos-1] = setDict(dic, keys=fullKeys[dic['name']]['mandatory'] + fullKeys[dic['name']]['optional'][0] + ['name'], default=fullKeys[dic['name']]['mandatory'] + fullKeys[dic['name']]['optional'][1] + [dic['name']])
+            listProfiles[pos-1] = setDict(dic, keys=fullKeys[dic['name']]['parameters'] + ['name'], default=fullKeys[dic['name']]['default'] + [dic['name']])
     
     # Append each profile configuration into a variable of type str
     out      = genHeader(**removeKeys(header, keys=['name']))
@@ -205,34 +190,36 @@ def writeFeedmes(header, listProfiles, inputNames=[], outputNames=[], feedmeName
         file.write(out)
         file.close()
 
-def genHeader(outputImage, xmin, xmax, ymin, ymax,
-              inputImage="none", sigmaImage="none", psfImage="none", maskImage="none", couplingFile="none",
+def genHeader(inputImage="none", outputImage='output.fits', sigmaImage="none", psfImage="none", maskImage="none", couplingFile="none", 
+              xmin=0, xmax=100, ymin=0, ymax=100, sizeConvX=None, sizeConvY=None,
               psfSamplingFactor=1, zeroPointMag=30.0, arcsecPerPixel=[0.03, 0.03],
-              sizeConvX=None, sizeConvY=None, displayType="regular", option=0):
+              displayType="regular", option=0):
     """
     Constructs a galfit.feedme header as general as possible.
     
-    Mandatory inputs
-    ----------------
-        outputImage : string
-            name of the file within which the image will be stored. Technically, the output "image" is not an image but a (1+3)D fits file, where the layer 0 is a blank image whose header contains the fits keys, the layer 1 is the original image within the fitting region, layer 2 is the model image and layer 3 is the residual between the model and the original image. Note that the residual is computed by subtracting the layer 2 from the layer 1. Hence, if any galaxy component is missing in the model image (even if it was optimised), the residual will not reflect the "goodness" of fit.
+    Main inputs
+    -----------
+        inputImage : str
+            name of the input file (fits file only). Note that if "none" is given, when running the feedme file, the fitting will be skipped and a model will be generated using the provided parameters.
+        outputImage : str
+            name of the file within which the image will be stored. Default is 'output.fits'.
+            Technically, the output "image" is not an image but a (1+3)D fits file, where the layer 0 is a blank image whose header contains the fits keys, the layer 1 is the original image within the fitting region, layer 2 is the model image and layer 3 is the residual between the model and the original image. Note that the residual is computed by subtracting the layer 2 from the layer 1. Hence, if any galaxy component is missing in the model image (even if it was optimised), the residual will not reflect the "goodness" of fit.
+            
         xmax : int
-            maximum x coordinate of the fitting region box (in px)
+            maximum x coordinate of the fitting region box (in px). Default is 100 px.
         xmin : int
-            minimum x coordinate of the fitting region box (in px)
+            minimum x coordinate of the fitting region box (in px). Default is 0 px.
         ymax : int
-            maximum y coordinate of the fitting region box (in px)
+            maximum y coordinate of the fitting region box (in px). Default is 100 px.
         ymin : int
-            minimum y coordinate of the fitting region box (in px)
+            minimum y coordinate of the fitting region box (in px). Default is 0 px.
         
-    Optional inputs
-    ---------------
+    Additional inputs
+    -----------------
         arcsecPerPixel : list of two floats
             angular resolution (in arcsec) of image pixels. First element is the angular resolution in the x direction, second element in the y direction.
         displayType : "regular", "both" or "curses"
             galfit display mode. "Regular" mode does not allow any interaction. "Both" and "curses" modes allow you to interact with galfit during the fitting routine (both will display in a xterm terminal the possible commands).
-        inputImage : str
-            name of the input file (fits file only). Note that if "none" is given, when running the feedme file, the fitting will be skipped and a model will be generated using the provided parameters.
         couplingFile : str
             name of the .constraints file used to add constraints on parameters.
         maskImage : str
