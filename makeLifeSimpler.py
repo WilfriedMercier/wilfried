a3f87448d11322c0a6afe2537fddb0c11189be35
Wilfried Mercier <wilfried.mercier58@gmail.com>
2020-11-17 17:16:00 +0100

    Integrated back the transpose functionnality and added a fonction to estimate the error based on the computation of some function given its parameters errors

--- makeLifeSimpler.py
+++ makeLifeSimpler.py
@@ -8,7 +8,109 @@ Created on Tue Apr  2 09:32:44 2019
 A set of useful functions to make life simpler when analysing data.
 """
 
-import numpy as np
+import numpy        as np
+import numpy.random as rand
+
+
+def inferError(function, parameters, err_params, minBounds=None, maxBounds=None, num=1000, centralValue=None, **kwargs):
+    '''
+    Infer the 1 sigma error on the output of a function by estimating its standard deviation when perturbating its parameters according to their errors.
+    Errors are assumed to be Gaussian.
+
+    Mandatory parameters
+    --------------------
+        function : func
+            function to infer the output error
+        parameters : list
+            parameters to pass to the function. Order must be identical as in the function declaration.
+        err_params : list
+            1 sigma errors of the parameters. If the parameter is fixed or perfectly known, provide None instead to bypass the pertubation part.
+            
+    Optional parameters
+    -------------------
+        centralValue : int/float
+            value used to compute the standard deviation (usually median or mean). If no value is provided, the median value of the perturbed iterations is used.
+        num : int
+            number of iterations to perform
+        maxBounds : list
+            maximum limit allowed for the parameters when perturbating them. All perturbed parameters beyond these thresholds will be set to these values.
+        minBounds ; list
+            minimum limit allowed for the parameters when perturbating them. All perturbed parameters beyond these thresholds will be set to these values.
+        **kwargs : dict
+            additional parameters to pass onto the function at the end
+
+    Return the 1 sigma error of the function around the median value.
+    '''
+    
+    if not isinstance(num, int):
+        raise TypeError('Number of iterations must be an integer.')
+        
+    if not isinstance(parameters, list):
+        raise TypeError('Parameters must be given as a list.')
+    
+    if not isinstance(err_params, list):
+        raise TypeError('Parameters errors must be given as a list.')
+        
+    if minBounds is None:
+        minBounds = [None]*len(parameters)
+    if maxBounds is None:
+        maxBounds = [None]*len(parameters)
+
+    if not isinstance(minBounds, list):
+        raise TypeError('Minimum bounds should be given as a list.')
+
+    if not isinstance(maxBounds, list):
+        raise TypeError('Maximum bounds should be given as a list.')
+        
+    # Generating the perturbations for each parameter
+    perturbations = []
+    for value, error, mini, maxi in zip(parameters, err_params, minBounds, maxBounds):
+        
+        # Without error, we had the parameter value without perturbations
+        if error is None:
+            perturbations.append([value]*num)
+        else:
+            param = rand.normal(loc=value, scale=error, size=num)
+            
+            # If there is a min or max bound, we clip outliers
+            if mini is not None:
+                param[param<mini] = mini
+            if maxi is not None:
+                param[param>maxi] = maxi
+                
+            perturbations.append(param)
+    perturbations = transpose(perturbations)
+
+    # Apply the function to each element
+    result        = np.asarray([function(*i, **kwargs) for i in perturbations])
+    
+    if centralValue is None:
+        centralValue = np.nanmedian(result)
+        
+    if not isinstance(centralValue, (int, float)):
+        raise TypeError('Central value should either be an int or a float.')
+        
+    return np.nanstd(result-centralValue)
+    
+    
+
+def transpose(lists):
+    '''
+    Take the transpose of a list of lists/tuples.
+    All the inner lists are assumed to be of equal length, otherwise the transpose will not work correctly.
+
+    Parameters
+    ----------
+        lists : list of lists/tuples
+            list of all the lists or tuples to perform the transpose onto
+
+    Return the transposed list.
+    '''
+    
+    if not isinstance(lists, list):
+        raise TypeError('lists parameter should be of type list only. Cheers !')
+    
+    return list(zip(*lists))
 
 
 def convertCoords(coordinates, inSize=(200.0, 200.0), outSize=(31.0, 31.0), conversionFactor=1.0):
