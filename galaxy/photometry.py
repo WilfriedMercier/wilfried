29096cff2ca34dc78d7c7748e7ccdefde102979e
Wilfried Mercier <wilfried.mercier58@gmail.com>
2021-04-23 16:26:48 +0200

    upated docstrings for sphinx

--- galaxy/photometry.py
+++ galaxy/photometry.py
@@ -1,44 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec 15 15:50:50 2020
-
-@author: Benoit Epinat (LAM) & Wilfried Mercier (IRAP)
+.. codeauthor:: Epinat Benoit - LAM <benoit.epinat@lam.fr> & Wilfried Mercier - IRAP <wilfried.mercier@irap.omp.eu>
 
 Photometry functions related to galaxies.
 """
 
 import numpy             as np
+import astropy.units     as u
 from   astropy.cosmology import FlatLambdaCDM
 
+#: Cosmology used inthe functions
 cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
 
 def calzetti_law(lbda, rv=4.05, a=2.659, b1=-1.857, c1=1.040, d1=0, e1=0, b2=-2.156, c2=1.509, d2=-0.198, e2=0.011):
-    '''
-    Attenuation curve from Calzetti et al. (2000). The coefficients can be modified in input. 
-    In principle, this law is valid from 0.12 to 2.2 microns.
+    r'''
+    .. codeauthor:: Epinat Benoit - LAM <benoit.epinat@lam.fr>
     
-    The relation is k=a*(b+c/lbda+d/lbda**2+e/lbda**3)
+    Attenuation curve from Calzetti et al. (2000). The coefficients can be modified in input. In principle, this law is valid from 0.12 to 2.2 microns.
     
-    Author : Benoit Epinat - LAM
+    The relation is 
     
-    Parameters
-    ----------
-        lbda: float/numpy array
-            wavelength in microns
-    
-    Optional parameters
-    -------------------
-        rv: float
-            ratio of the total to selective attenuation
-        a: float
-            multiplicative coefficient
-        b1, c1, d1: float
-            coefficients for the relation above 0.63 microns
-        b2, c2, d2: float
-            coefficients for the relation below 0.63 microns
-            
-    Return the Calzetti attenuation.
+    .. math::
+
+        k = a \times (b + c/\lambda + d/\lambda^2 + e/\lambda^3),
+        
+    with :math:`\lambda` the wavelength.
+
+    :param lbda: wavelength in microns
+    :type lbda: float or ndarray[float]
+
+    :param float rv: (**Optional**) ratio of the total to selective attenuation
+    :param float a: (**Optional**) multiplicative coefficient
+    :param float bi: (**Optional**) coefficients for the relation above 0.63 microns
+    :param float bi: (**Optional**) coefficients for the relation below 0.63 microns
+        
+    :returns: Calzetti attenuation
+    :rtype: float or ndarray[float]
     '''
     
     lbda     = np.asarray(lbda)
@@ -54,45 +52,39 @@ def calzetti_law(lbda, rv=4.05, a=2.659, b1=-1.857, c1=1.040, d1=0, e1=0, b2=-2.
 
 def dust_attenuation_calzetti(lbda, ebv, rv=4.05):
     '''
+    . codeauthor:: Epinat Benoit - LAM <benoit.epinat@lam.fr>
+    
     Compute the dust attenuation using the Calzetti attenuation curve (could be more general adding a function name) from the colour excess.
-    
-    Author : Benoit Epinat - LAM
-    
-    Parameters
-    ----------
-        lbda: float/numpy array
-            wavelength where the attenuation has to be estimated
-        ebv: float
-            colour excess
-    
-    Optional parameters
-    -------------------
-        rv: float
-            ratio of the total to selective attenuation
-    
-    Return the dust attenuation in mag.
+
+    :param lbda: wavelength in microns
+    :type lbda: float or ndarray[float]
+    :param float ebv: colour excess
+
+    :param float rv: (**Optional**) ratio of the total to selective attenuation
+
+    :returns: dust attenuation in mag
+    :rtype: float or ndarray[float]
     '''
     
     return ebv * calzetti_law(lbda, rv=rv)
 
 def cardelli_law(lbda, rv=3.1):
     '''
+    .. codeauthor:: Epinat Benoit - LAM <benoit.epinat@lam.fr>
+    
     Attenuation curve from Cardelli et al. (1989). The coefficients can be modified in input. 
-    In principle, this law is valid from 0.1 to 3.3 microns. For MW, Rv~3.1.
     
-    Author : Benoit Epinat - LAM
+    .. note::
+        
+        In principle, this law is valid from 0.1 to 3.3 microns. For MW, Rv~3.1.
     
-    Parameters
-    ----------
-        lbda: float/numpy array
-            wavelength in microns
+    :param lbda: wavelength in microns
+    :type lbda: float or ndarray[float]
+    
+    :param float rv: (**Optional**) ratio of the total to selective attenuation
             
-    Optional parameters
-    -------------------
-        rv: float
-            ratio of the total to selective attenuation
-            
-    Return the cardelli attenuation.
+    :returns: cardelli attenuation
+    :rtype: float or ndarray[float]
     '''
     
     # Need to be sure we work with numpy arrays to have the right behaviour for the bineary operators
@@ -132,42 +124,39 @@ def cardelli_law(lbda, rv=3.1):
 
 def dust_attenuation_cardelli(lbda, ebv, rv=3.1):
     '''
+    .. codeauthor:: Epinat Benoit - LAM <benoit.epinat@lam.fr>
+    
     Compute the dust attenuation using the Cardelli attenuation curve from the colour excess.
-    
-    Author : Benoit Epinat - LAM
-    
-    Parameters
-    ----------
-        lbda: float/numpy array
-            wavelength where the attenuation has to be estimated
-        ebv: float/numpy array
-            colour excess
+
+    :param lbda: wavelength in microns
+    :type lbda: float or ndarray[float]
+    :param float ebv: colour excess
+
+    :param float rv: (**Optional**) ratio of the total to selective attenuation
             
-    Optional parameters
-    -------------------
-        rv: float
-            ratio of the total to selective attenuation
-            
-    Return the dust attenuation in mag.
+    :returns: dust attenuation in mag
+    :rtype: float or ndarray[float]
     '''
     
     return ebv * cardelli_law(lbda, rv=rv) 
 
 def flux_to_lum(floii, z):
-    '''
+    r'''
+    .. codeauthor:: Epinat Benoit - LAM <benoit.epinat@lam.fr>
+    
     Convert a flux into a luminosity.
+
+    :param floii: flux in :math:`\rm{erg/s/cm^2}`. Optionally, can directly be given as an Astropy Quantity with a unit of the kind :math:`\rm{erg/s/cm^2}`.
+    :type floii: float or ndarray[float]
+    :param z: redshift
+    :type z: float or ndarray[float]
     
-    Author : Benoit Epinat - LAM
-    
-    Parameters
-    ----------
-        floii: float/numpy array
-            flux in erg/s/cm^2. Optionally, can e directly given as an Astropy Quantity with a unit of the kind erg/s/cm^2.
-        z : float/numpy array
-            redshift
-    
-    Returns luminosity in erg/s.
+    :returns: luminosity in :math:`\rm{erg/s}`
+    :rtype: float or ndarray[float]
     '''
+    
+    if not hasattr(floii, 'unit'):
+        floii = u.Quantity(floii, unit='erg/(s.cm^2)')
     
     dl   = cosmo.luminosity_distance(z)
     loii = (floii * 4 * np.pi * dl**2).to('erg/s').value
@@ -177,25 +166,24 @@ def flux_to_lum(floii, z):
 
 def correct_extinction(floii, z, av_fast, ebv, lbda=0.3728):
     '''
+    .. codeauthor:: Epinat Benoit - LAM <benoit.epinat@lam.fr>
+    
     Correct for both galactic extinction and internal extinction.
+
+    :param av_fast: attenuation from FAST
+    :type av_fast: float or ndarray[float]
+    :param ebv: colour excess
+    :type ebv: float or ndarray[float]
+    :param floii: ionised gas flux
+    :type floii: float or ndarray[float]
+    :param z: redshift
+    :type z: float or ndarray[float]
+
+    :param lbda: (**Optional**) rest-frame wavelength in microns
+    :type lbda: float or ndarray[float]
     
-    Author : Benoit Epinat - LAM
-    
-    Parameters
-    ----------
-        av_fast : float/numpy array
-            attenuation from FAST
-        ebv : float/numpy array
-            colour excess
-        floii : float/numpy array
-            ionised gas flux
-        z : float/numpy array
-            redshift
-    
-    Optional parameters
-    -------------------
-        lbda : float/numpy array
-            rest-frame wavelength in microns
+    :returns: flux corrected of extinction
+    :rtype: float or ndarray[float]
     '''
     
     if hasattr(lbda, 'unit'):
@@ -206,11 +194,11 @@ def correct_extinction(floii, z, av_fast, ebv, lbda=0.3728):
     # Galactic extinction at the observed wavelength
     abs_gal        = dust_attenuation_cardelli(lbda_obs, ebv, rv=3.1)
     
-    # Convert the extinction from FAST at the observed wavelength
+    # Convert the extinction from FAST to the observed wavelength
     ext_fast       = av_fast / calzetti_law(np.array(.550))
     abs_fast       = dust_attenuation_calzetti(lbda, ext_fast, rv=4.05)
     
-    # Apply the flux correction from the Milky Way and the galaxy extinction
+    # Apply the flux correction from the Milky Way and the intrinsic extinction
     flux_corr_gal  = floii * 10 ** (0.4 * abs_gal)
     flux_corr_fast = flux_corr_gal * 10 ** (0.4 * abs_fast)
     
@@ -222,49 +210,49 @@ def correct_extinction(floii, z, av_fast, ebv, lbda=0.3728):
 #################################################### 
 
 def mass_gas_sfr(sfr, rd):
-    '''
-    Convert the SFR into a gas mass assuming it is uniformly distributed in 2.2*rd.
+    r'''
+    .. codeauthor:: Epinat Benoit - LAM <benoit.epinat@lam.fr>
     
-    Author : Benoit Epinat - LAM
-    
-    Parameters
-    ----------
-        sfr : float/numpy array  
-            SFR in Msun/yr. Optionally, if given as an astropy Quantity, it will be converted to Msun/yr.
-        rd : float/numpy array
-            disk scale length in pc. Optionally, if given as an astropy Quantity, it will be converted to pc.
-    
-    Return the mass gas.
+    Convert the SFR into a gas mass assuming it is uniformly distributed in :math:`2.2 \times R_{\rm{d}}`, with :math:`R_{\rm{d}}` the disk scale length.
+
+    :param sfr: SFR in :math:`M_{\odot}/{\rm{yr}}`. Optionally, if given as an astropy Quantity, it will be converted to :math:`M_{\odot}/{\rm{yr}}`.
+    :type sfr: float or ndarray[float]
+    :param rd: disk scale length in :math:`\rm{pc}`. Optionally, if given as an astropy Quantity, it will be converted to :math:`\rm{pc}`.
+    :type rd: float or ndarray[float]
+
+    :returns: gas mass in :math:`M_{\odot}`
+    :rtype: float or ndarray[float]
     '''
     
     # Astropy Quantities
     if hasattr(rd, 'unit'):
-        rd.to('pc')
+        rd.to('pc').value
     
     if hasattr(sfr, 'unit'):
-        sfr.to('solMass/yr')
+        sfr.to('solMass/yr').value
     
     return (np.pi * (2.2*rd)**2) ** (2/7) * (sfr / 2.5e-10) ** (5/7)
 
 
 def mass_gas_kennicutt(floii, rd, z):
-    '''
+    r'''
+    .. codeauthor:: Epinat Benoit - LAM <benoit.epinat@lam.fr>
+    
     Convert the ionised gas flux into a gas mass assuming it is uniformly distributed in 2.2*rd.
     
-    Author : Benoit Epinat - LAM
+    :param floii: ionised gas flux in :math:`\rm{erg/s/cm2}`
+    :type floii: float or ndarray[float]
+    :param rd: disk scale length in :math:`\rm{pc}`. Optionally, if given as an astropy Quantity, it will be converted to :math:`\rm{pc}`.
+    :type rd: float or ndarray[float]
+    :param z: redshift
+    :type z: float or ndarray[float]
     
-    Parameters
-    ----------
-        floii : float/numpy array
-            ionised gas flux in erg/s/cm2
-        rd : float/numpy array
-            disk scale length in pc. Optionally, if given as an astropy Quantity, it will be converted to pc.
-    
-    Return the mass gas using the Kennicutt law and its error.
+    :returns: gas mass using the Kennicutt law and its error in :math:`M_{\odot}`
+    :rtype: float or ndarray[float], float or ndarray[float]
     '''
     
     if hasattr(rd, 'unit'):
-        rd.to('pc')
+        rd.to('pc').value
     
     loii  = flux_to_lum(floii, z)
     mgas  = ((np.pi * (2.2*rd)**2) ** (2/7) * 4.7564e-23 * loii ** (5/7)).value
@@ -274,19 +262,17 @@ def mass_gas_kennicutt(floii, rd, z):
 
 
 def sfr_kennicutt_oii(floii, z):
-    '''
+    r'''
+    .. codeauthor:: Epinat Benoit - LAM <benoit.epinat@lam.fr>
+    
     Compute the SFR from the ionised gas flux.
     
-    Author : Benoit Epinat - LAM
+    :param floii: ionised gas flux
+    :type floii: float or ndarray[float]
+    :param z: redshift
+    :type z: float or ndarray[float]
     
-    Parameters
-    ----------
-        floii : float/numpy array
-            ionised gas flux in erg/s/cm2
-        z : float/numpy array
-            redshift
-    
-    Return the SFR in Msun/yr and its error.
+    :returns: SFR in :math:`M_{\odot}/{\rm{yr}}` and its error.
     '''
     
     loii = flux_to_lum(floii, z)
