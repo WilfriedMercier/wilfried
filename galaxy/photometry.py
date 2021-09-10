2ca55bf3d1dcffde0f53b91cc8cc843a647e70c1
Wilfried Mercier <wilfried.mercier58@gmail.com>
2021-09-10 17:38:36 +0200

    added two functions to convert beween photon counts and mag/fluxes

--- galaxy/photometry.py
+++ galaxy/photometry.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
+r"""
 .. codeauthor:: Epinat Benoit - LAM <benoit.epinat@lam.fr> & Wilfried Mercier - IRAP <wilfried.mercier@irap.omp.eu>
 
 Photometry functions related to galaxies.
@@ -12,6 +12,64 @@ from   astropy.cosmology import FlatLambdaCDM
 
 #: Cosmology used inthe functions
 cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+
+
+###############################################
+#       Flux and magnitudes conversions       #
+###############################################
+
+def countToMag(data, err, zeropoint):
+    r'''
+    Convert data counts and their associated error into AB magnitudes using the formula
+    
+    .. math::
+        
+        d [{\rm{mag}}] = -2.5 \log_{10} d [{\rm{e^{-}/s}}] + {\rm{zpt}}
+        
+    where :math:`d` is the data and :math:`\rm{zpt}` is the magnitude zeropoint. The error is given by
+    
+    .. math::
+        
+        \Delta d [{\rm{mag}}] = 1.08 \Delta d [{\rm{e^{-}/s}}] / d [{\rm{e^{-}/s}}]
+    
+    :param data: data in electron/s
+    :type data: float or ndarray[float]
+    :param err: std errors in electron/s
+    :type err: float or ndarray[foat]
+    :param float zeropoint: zeropoint associated to the data
+    
+    :returns: AB magnitude and associated error
+    :rtype: float or ndarray[float], float or ndarray[float]
+    '''
+
+    mag  = -2.5 * np.log10(data) + zeropoint
+    emag = 1.08 * err/data
+    
+    return mag, emag
+
+def countToFlux(data, err, zeropoint):
+    r'''
+    Convert data counts and their associated error into flux in :math:`erg/cm^2/s/Hz`.
+    
+    :param data: data in :math:`electron/s`
+    :type data: float or ndarray[float]
+    :param err: std errors in :math:`electron/s`
+    :type err: float or ndarray[float]
+    :param float zeropoint: zeropoint associated to the data
+    
+    :returns: AB magnitude and associated error
+    :rtype: float or ndarray[float], float or ndarray[float]
+    '''
+
+    flux  = data * 10**(-(zeropoint+48.6)/2.5)
+    eflux = err  * 10**(-(zeropoint+48.6)/2.5)
+    
+    return flux, eflux
+
+
+###############################################
+#       Dust extinction and attenuation       #
+###############################################
 
 def calzetti_law(lbda, rv=4.05, a=2.659, b1=-1.857, c1=1.040, d1=0, e1=0, b2=-2.156, c2=1.509, d2=-0.198, e2=0.011):
     r'''
