a45208327a145d58597b188fc88f5dc94c3b7f08
Wilfried Mercier <wilfried.mercier58@gmail.com>
2022-02-17 16:54:35 +0100

    update

--- makeLifeSimpler.py
+++ makeLifeSimpler.py
@@ -9,28 +9,42 @@ A set of useful functions to make life simpler when analysing data.
     Some functions have not been updated or used in months and may not behave as would be expected.
 """
 
+from   numpy        import ndarray
+from   typing       import Callable, List, Union, Tuple, Dict
 from   scipy.stats  import median_abs_deviation
 import numpy        as     np
 import numpy.random as     rand
 
 
-def inferError(function, parameters, err_params, minBounds=None, maxBounds=None, num=1000, centralValue=None, **kwargs):
-    '''
+def inferError(function: Callable, parameters: List, err_params: List, 
+               minBounds: List=None, maxBounds: List=None, num: int=1000, centralValue: Union[int, float]=None, 
+               **kwargs) -> float:
+    r'''
     Infer the 1 sigma error on the output of a function by estimating its standard deviation when perturbating its parameters according to their errors.
     Errors are assumed to be Gaussian.
 
     :param func function: function to infer the output error
     :param list parameters: parameters to pass to the function. Order must be identical as in the function declaration.
     :param list err_params: 1 sigma errors of the parameters. If the parameter is fixed or perfectly known, provide None instead to bypass the pertubation part.
-    :param centralValue: value used to compute the standard deviation (usually median or mean). If no value is provided, the median value of the perturbed iterations is used.
+    
+    :param list minBounds: (**Optional**) minimum limit allowed for the parameters when perturbating them. All perturbed parameters beyond these thresholds will be set to these values.
+    :param list maxBounds: (**Optional**) maximum limit allowed for the parameters when perturbating them. All perturbed parameters beyond these thresholds will be set to these values.
+    :param centralValue: (**Optional**) value used to compute the standard deviation (usually median or mean). If no value is provided, the median value of the perturbed iterations is used.
     :type centralValue: int or float
-    :param int num: number of iterations to perform
-    :param list maxBounds: maximum limit allowed for the parameters when perturbating them. All perturbed parameters beyond these thresholds will be set to these values.
-    :param list minBounds: minimum limit allowed for the parameters when perturbating them. All perturbed parameters beyond these thresholds will be set to these values.
-    :param dict **kwargs: additional parameters to pass onto the function at the end
+    :param int num: (**Optional**) number of iterations to perform
+    :param dict kwargs: additional parameters to pass onto the function at the end
+    
     :returns: 1 sigma error of the function around the median value
     :rtype: float
-    :raises TypeError: if num is not an int, or if parameters, err_params, minBounds or maxBounds are not lists, or if centralValue is neither int nor float
+    
+    :raises TypeError: if 
+    
+    * **num** is not an int
+    * **parameters** is not a list
+    * **err_params** is not a list 
+    * **minBounds** is not a list 
+    * **maxBounds** is not a list 
+    * **centralValue** is neither int nor float
     '''
     
     if not isinstance(num, int):
@@ -83,15 +97,15 @@ def inferError(function, parameters, err_params, minBounds=None, maxBounds=None,
         
     return median_abs_deviation(result-centralValue, axis=None, nan_policy='omit')
     
-    
-
-def transpose(lists):
-    '''
+def transpose(lists: List[Union[List, Tuple]]) -> List[Union[List, Tuple]]:
+    r'''
     Take the transpose of a list of lists/tuples.
+    
     All the inner lists are assumed to be of equal length, otherwise the transpose will not work correctly.
 
     :param lists: list of all the lists or tuples to perform the transpose onto
     :type lists: list[list] or lists[tuple]
+    
     :returns: the transposed list
     :rtype: list
     '''
@@ -102,17 +116,21 @@ def transpose(lists):
     return list(zip(*lists))
 
 
-def convertCoords(coordinates, inSize=(200, 200), outSize=(31, 31), conversionFactor=1.0):
-    '''
+def convertCoords(coordinates: Union[List, List[Dict]], 
+                  inSize: Tuple[int] = (200, 200), outSize: Tuple[int] = (31, 31), 
+                  conversionFactor: float = 1.0) -> List[Dict]:
+    r'''
     Transforms the coordinates of a/many point(s) from one image to another
     
     :param coordinates: coordinates of the points to convert from one image to another
     :type coordinates: dict or list of dict
-    :param float conversionFactor: numerical factor to convert the position from pixel to another relavant unit
-    :param inSize: size of the image the points are from
+    
+    :param inSize: (**Optional**) size of the image the points are from
     :type inSize: 2-tuple[int] or list[int]
-    :param outSize: size of the image whereto we want to convert the positions of the points
+    :param outSize: (**Optional**) size of the image whereto we want to convert the positions of the points
     :type outSize: 2-tuple[int] or list[int]
+    :param float conversionFactor: (**Optional**) numerical factor to convert the position from pixel to another relavant unit
+    
     :returns: a list of dictionnaries with transformed coordinates
     :rtype: list[dict]
     '''
@@ -125,80 +143,26 @@ def convertCoords(coordinates, inSize=(200, 200), outSize=(31, 31), conversionFa
     for num, points in enumerate(coordinates):
         for pos, key in enumerate(points.keys()):
             coordinates[num][key] *= outSize[pos]/inSize[pos]*conversionFactor
+            
     return coordinates
 
 
-
-def printSimpleStat(catalog, unit=None):
-    """
-    Print basic stats such as median and mean values, as well as 1st and 3rd quantiles.
-    
-    :param catalog: array from which the statistic is computed
-    :type catalog: ndarray or astropy table or list or list[ndarray] or list[astropy table] or list[list]
-    :param unit: unit of the array if there is one
-    :type unit: astropy unit
-    :returns: None
-    """
-
-    try:
-        np.shape(catalog[1])
-    except IndexError:
-        catalog = [catalog]
-    
-    for cat, num in zip(catalog, range(len(catalog))):
-        if unit is not None:
-            cat = cat*unit
-            
-        print("Stat for catalog number", num, ":")
-        print("Maximum separation is", str(np.max(cat)) + ".")
-        print("Mean separation is", str(np.mean(cat)) + ".")
-        print("Median separation is", str(np.median(cat)) + ".")
-        print("1st quantile is", str(np.quantile(cat, 0.25)) + ".")
-        print("3rd quantile is", str(np.quantile(cat, 0.75)) + ".")
-        
-    return   
-   
-
-def uniqueArr(tables, arraysToBeUnique):
-    """
-    Apply a mask from np.unique on arraysToBeUnique for many arrays.
-    
-    :param tables: tables to which the mask is applied
-    :type tables: astropy table or ndarray or list[astropy table] or list[ndarray]
-    :param arraysToBeUnique: tables or arrays from which the mask is computed (with np.unique)
-    :type arraysToBeUnique: astropy table or ndarray or list[astropy table] or list[array]
-    :returns: tables with the mask applied
-    :rtype: Same as tables
-    """
-    
-    #Transform into a list if it is an array
-    try:
-        np.shape(tables[1])
-    except IndexError:
-        tables = [tables]
-    try:
-        np.shape(arraysToBeUnique[1])
-    except IndexError:
-        arraysToBeUnique = [arraysToBeUnique]
-        
-    for num, uniq in zip(range(len(tables)), arraysToBeUnique):    
-        arr, indices = np.unique(uniq, return_index=True)
-        tables[num]  = tables[num][indices]
-        
-    return tables
-
-
-def maskToRemoveVal(listOfArrays, val=None, keep=True, astroTableMask=False):
-    """
+def maskToRemoveVal(listOfArrays: List[ndarray], 
+                    val: Union[int, float] = None, keep: bool = True, astroTableMask: bool = False) -> ndarray:
+    r"""
     Computes a mask by finding occurences in a list of arrays.
     
     :param list[ndarray] listOfArrays: list of arrays from which the mask is built
-    :param val: value to find. If None, it looks for nan values.
+    
+    :param val: (**Optional**) value to find. If None, it looks for nan values.
     :type val: int or float or None
-    :param bool keep: if True, it builds a mask with True everywhere the value val is encountered. If False, it does the opposite.
-    :param bool astroTableMask: if True returns a mask from the astropy table column instead of looking for some value/nans with False values everywhere the data is masked.
+    :param bool keep: (**Optional**) if True, it builds a mask with True everywhere the value val is encountered. If False, it does the opposite.
+    :param bool astroTableMask: (**Optional**) if True returns a mask from the astropy table column instead of looking for some value/nans with False values everywhere the data is masked.
+    
     :returns: a mask
     :rtype: ndarray
+    
+    :raises ValueError: if the arrays do not have the same shape
     """
     
     shp = listOfArrays[0].shape
@@ -232,12 +196,13 @@ def maskToRemoveVal(listOfArrays, val=None, keep=True, astroTableMask=False):
     return tmp
 
 
-def logicalAndFromList(lst):
-    """
+def logicalAndFromList(lst: List[ndarray]) -> ndarray:
+    r"""
     Compute the intersection of all the subarrays in the main array
     
-    :param lst: list of arrays containing True of False values
+    :param lst: list of arrays containing True or False values
     :type lst: list[ndarray]
+    
     :returns: np.logical_and applied on all the subarrays
     :rtype: ndarray
     """
@@ -249,12 +214,13 @@ def logicalAndFromList(lst):
     return tmp
 
 
-def applyMask(listOfArrays, mask):
-    """
+def applyMask(listOfArrays: List[ndarray], mask: ndarray) -> List[ndarray]:
+    r"""
     Apply the same mask to a list of arrays and return the new arrays.
     
     :param list[ndarray] listOfArrays: list of arrays the mask is applied to
     :param ndarray mask: mask to apply
+    
     :returns: list of arrays with the mask applied. If len(listOfArrays) is 1, it returns only an array instead of a list of arrays with one object.
     :rtype: list[ndarray] if len(listOfArrays) != 1 else ndarray
     """
@@ -264,16 +230,19 @@ def applyMask(listOfArrays, mask):
             listOfArrays = array[mask]
         else:
             listOfArrays[num] = array[mask]
+
     return listOfArrays
 
 
-def findWhereIsValue(listOfArrays, val=None):
+def findWhereIsValue(listOfArrays: List[ndarray], val: Union[int, float] = None) -> List[bool]:
     """
     Find and print the first position where a value is found within a list of arrays.
     
     :param list[ndarray] listOfArrays: list from which the value val is searched
-    :param val: value to look for. If None, it looks for nan values.
+    
+    :param val: (**Optional**) value to look for. If None, it looks for nan values.
     :type val: int or float or None
+    
     :returns: list of booleans with the same length as listOfArrays, with True when the value was found in the array and False otherwise.
     :rtype: list[bool]
     """
@@ -298,13 +267,15 @@ def findWhereIsValue(listOfArrays, val=None):
     return returnArr
                 
 
-def checkDupplicates(master, names=None):
+def checkDupplicates(master: List[ndarray], names: List[str] = None) -> None:
     """
     Check if galaxies are found multiple times in an array by looking for duplicates of (RA, DEC) pairs.
     
     :param master: list of structured arrays to check
     :type master: list of structured ndarrays (with 'RA' and 'DEC' fields)
-    :param list[str] names: names of the arrays
+    
+    :param list[str] names: (**Optional**) names of the arrays
+    
     :returns: None
     """
     
